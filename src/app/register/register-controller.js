e5681f088e09f9ed1b110bb63c3fce8a66d2ecf4
daniyal91 <raothegreat_dani_91@hotmail.com>
2016-11-19 05:57:53 -0500

    Username and email validation

--- src/app/register/register-controller.js
+++ src/app/register/register-controller.js
@@ -4,6 +4,7 @@ angular.module('app').controller('RegisterCtrl',function($scope,userService){
 	$scope.registerError = false;
 	$scope.passwordMatchError = false;
 	$scope.userRegistered = false;
+	$scope.userNotRegistered = false;
 
 	$scope.register = function() {
 		if($scope.newUser.firstName !== "" && $scope.newUser.lastName !== "" && $scope.newUser.email !== "" && $scope.newUser.username !== "" && $scope.newUser.password !== "" && $scope.newUser.confirmPassword !== "" && $scope.newUser.gender !== "" && $scope.newUser.language !== "" && $scope.newUser.address !== "" && $scope.newUser.type !== "")
@@ -17,7 +18,15 @@ angular.module('app').controller('RegisterCtrl',function($scope,userService){
 			{
 				$scope.passwordMatchError = false;
 				userService.resource.save($scope.newUser, function(data){
-					$scope.userRegistered = true;
+					if(data.message[0])
+					{
+						$scope.errorMsg = data.message[0];
+						$scope.userNotRegistered = true;
+					}
+					else
+					{
+						$scope.userRegistered = true;
+					}
 				});
 			}
 		}
